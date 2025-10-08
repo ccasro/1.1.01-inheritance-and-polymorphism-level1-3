6f589c5cdef6a9b0f56a3a24d17f157abf2dfd86
ccasro <ccasro@gmail.com>
2025-10-08 11:36:41 +0200

    feat(level1/exercise2) add complete Exercise 2 from level1

---
+++ inheritance-polymorphism/src/main/java/org/ccasro/level1/exercise2/Car.java
@@ -0,0 +1,20 @@
+package main.java.org.ccasro.level1.exercise2;
+
+public class Car {
+    private static final String brand = "Seat";
+    private static String model;
+    private final int power;
+
+    public Car(String model, int power) {
+        Car.model = model;
+        this.power = power;
+    }
+
+    public static void brake() {
+        System.out.println("The vehicle is braking");
+    }
+
+    public void accelerate() {
+        System.out.println("The vehicle is accelerating");
+    }
+}
--- inheritance-polymorphism/src/main/java/org/ccasro/level1/exercise2/MainL1E2.java
+++ inheritance-polymorphism/src/main/java/org/ccasro/level1/exercise2/MainL1E2.java
@@ -1,6 +1,12 @@
-package org.ccasro.level1.exercise2;
+package main.java.org.ccasro.level1.exercise2;
 
 public class MainL1E2 {
     public static void main(String[] args) {
+        System.out.println("invoking static method");
+        Car.brake();
+
+        System.out.println("invoking non-static method");
+        Car car = new Car("Seat", 110);
+        car.accelerate();
     }
 }
